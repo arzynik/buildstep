89ae95db2953f0644667e8150a357a39faa425d4
Devin Smith <github@arzynik.com>
2015-11-03 15:44:44 -0800

    updated for better awesomeness

--- run.sh
+++ run.sh
@@ -4,20 +4,32 @@ if [ "$#" -eq 0 ]; then
         exit 1
 fi
 
+if [[ $TUTUM_TRIGGER_BODY =~ ^GIT_BRANCH:.*$ ]]; then
+	echo "Using \$GIT_BRANCH from \$TUTUM_TRIGGER_BODY"
+	GIT_BRANCH=$(echo "$TUTUM_TRIGGER_BODY" | sed -E 's#GIT_BRANCH:(.*)#\1#g')
+fi
+
+if [[ $TUTUM_TRIGGER_BODY =~ ^GIT_VERSION:.*$ ]]; then
+	echo "Using \$GIT_VERSION from \$TUTUM_TRIGGER_BODY"
+	GIT_BRANCH=$(echo "$TUTUM_TRIGGER_BODY" | sed -E 's#GIT_VERSION:(.*)#\1#g')
+fi
+
 if [ ! -d "/app" ]; then
 	if [ -n "$GIT_REPO" ]; then
-		if [ -n "$GIT_BRANCH" ]; then
-			git clone -B "$GIT_BRANCH" "$GIT_REPO" /app
-		else
-			echo "No \$GIT_BRANCH environment variable defined, assuming MASTER"
+		if [ -n "$GIT_VERSION" ]; then
 			git clone "$GIT_REPO" /app
-		fi
-		if [ -n "$GIT_TAG" ]; then
-			git --git-dir /app/.git checkout tags/"$GIT_TAG"
-		elif [ -n "$GIT_VERSION" ]; then
-			git --git-dir /app/.git checkout "$GIT_VERSION"
+			git --git-dir /app/.git fetch
+			git --git-dir /app/.git checkout $GIT_VERSION
+		else
+			if [ -n "$GIT_BRANCH" ]; then
+				git clone --single-branch --depth 1 -b "$GIT_BRANCH" "$GIT_REPO" /app
+			else
+				echo "No \$GIT_BRANCH environment variable defined, assuming MASTER"
+				git clone --single-branch --depth 1 "$GIT_REPO" /app
+			fi
 		fi
 		/build/builder
+
 	else
 		echo "No \$GIT_REPO environment variable defined"
 		exit 1
